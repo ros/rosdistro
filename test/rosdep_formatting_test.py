3b6526bf324aa9865665b271953e4e6e61dfb7ff
Dirk Thomas <dthomas@osrfoundation.org>
2013-06-12 16:54:27 -0700

    improve messages of unit tests

--- test/rosdep_formatting_test.py
+++ test/rosdep_formatting_test.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 
-from scripts.check_rosdep import main as check_rosdep
 import os
+
+from scripts.check_rosdep import main as check_rosdep
 
 
 def test():
-    files= os.listdir('rosdep')
+    files = os.listdir('rosdep')
 
-    print """Running scripts/check_rosdep.py on all *.yaml in the rosdep directory.
-If this fails you can run scripts/clean_rosdep.py to help cleanup"""
-
+    print("""
+Running 'scripts/check_rosdep.py' on all *.yaml in the rosdep directory.
+If this fails you can run 'scripts/clean_rosdep.py' to help cleanup.
+""")
 
     for f in files:
         fname = os.path.join('rosdep', f)
         if not f.endswith('.yaml'):
-            print "Skipping rosdep check of file %s"%fname
+            print("Skipping rosdep check of file %s" % fname)
             continue
-        print "Checking rosdep file %s" % fname
+        print("Checking rosdep file %s" % fname)
         assert check_rosdep(fname)
-
-
