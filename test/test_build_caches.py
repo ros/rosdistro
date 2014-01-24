00f082f3ce3f273ac651d360113043e0919facc5
Dirk Thomas <dthomas@osrfoundation.org>
2014-01-24 14:58:01 -0800

    update tests to use non-deprecated rosdistro api

--- test/test_build_caches.py
+++ test/test_build_caches.py
@@ -1,6 +1,6 @@
 import os
 
-from rosdistro.release_cache_generator import generate_release_caches
+from rosdistro.distribution_cache_generator import generate_distribution_caches
 
 INDEX_YAML = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'index.yaml'))
 
@@ -11,4 +11,4 @@ Checking if the package.xml files for all packages are fetchable.
 If this fails you can run 'rosdistro_build_cache index.yaml' to perform the same check locally.
 """)
 
-    generate_release_caches(INDEX_YAML)
+    generate_distribution_caches(INDEX_YAML)
