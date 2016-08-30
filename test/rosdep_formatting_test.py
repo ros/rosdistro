5531896495438a744dda43d9a03f1b7e3bbba343
Dirk Thomas <dthomas@osrfoundation.org>
2016-08-30 14:56:56 -0700

    various test improvements

--- test/rosdep_formatting_test.py
+++ test/rosdep_formatting_test.py
@@ -9,14 +9,14 @@ def test():
     files = os.listdir('rosdep')
 
     print("""
-Running 'scripts/check_rosdep.py' on all *.yaml in the rosdep directory.
+Running 'scripts/check_rosdep.py' on all '*.yaml' in the 'rosdep' directory.
 If this fails you can run 'scripts/clean_rosdep.py' to help cleanup.
 """)
 
-    for f in files:
+    for f in sorted(files):
         fname = os.path.join('rosdep', f)
         if not f.endswith('.yaml'):
             print("Skipping rosdep check of file %s" % fname)
             continue
-        print("Checking rosdep file %s" % fname)
+        print("Checking rosdep file: %s" % fname)
         assert check_rosdep(fname)
