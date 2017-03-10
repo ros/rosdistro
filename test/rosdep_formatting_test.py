05dcc1fd6ba443e78a76137da6ce4a7bce48f25a
Dirk Thomas <dthomas@osrfoundation.org>
2017-03-10 14:51:41 -0800

    add folding of log sections for tests on Travis

--- test/rosdep_formatting_test.py
+++ test/rosdep_formatting_test.py
@@ -4,19 +4,21 @@ import os
 
 from scripts.check_rosdep import main as check_rosdep
 
+from .fold_block import Fold
+
 
 def test():
     files = os.listdir('rosdep')
 
-    print("""
-Running 'scripts/check_rosdep.py' on all '*.yaml' in the 'rosdep' directory.
+    with Fold():
+        print("""Running 'scripts/check_rosdep.py' on all '*.yaml' in the 'rosdep' directory.
 If this fails you can run 'scripts/clean_rosdep.py' to help cleanup.
 """)
 
-    for f in sorted(files):
-        fname = os.path.join('rosdep', f)
-        if not f.endswith('.yaml'):
-            print("Skipping rosdep check of file %s" % fname)
-            continue
-        print("Checking rosdep file: %s" % fname)
-        assert check_rosdep(fname)
+        for f in sorted(files):
+            fname = os.path.join('rosdep', f)
+            if not f.endswith('.yaml'):
+                print("Skipping rosdep check of file %s" % fname)
+                continue
+            print("Checking rosdep file: %s" % fname)
+            assert check_rosdep(fname)
