ae3d34bcb02ff0ebb35e6d3e8c08ac171fa6d839
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2017-07-08 11:24:59 +0900

    follow PEP 8 for the comments (two spaces + # + one space + comment text)

--- test/rosdep_duplicates_test.py
+++ test/rosdep_duplicates_test.py
@@ -9,8 +9,8 @@ from .fold_block import Fold
 
 def test():
     files = os.listdir('rosdep')
-    files = filter(lambda x: x.endswith('.yaml'), files) # accept only file ends with .yaml
-    files = [os.path.join('rosdep', x) for x in files] # use relative path
+    files = filter(lambda x: x.endswith('.yaml'), files)  # accept only file ends with .yaml
+    files = [os.path.join('rosdep', x) for x in files]  # use relative path
     with Fold() as fold:
         print("""Running 'scripts/check_duplicates.py' on all '*.yaml' in the 'rosdep' directory.
 """)
