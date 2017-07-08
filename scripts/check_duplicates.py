279f59f74d58d411dd7b374e126e3d002c10006e
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2017-07-08 11:18:08 +0900

    avoid colorization logic

--- scripts/check_duplicates.py
+++ scripts/check_duplicates.py
@@ -33,44 +33,6 @@ import os
 import sys
 import yaml
 
-# pretty - A miniature library that provides a Python print and stdout
-# wrapper that makes colored terminal text easier to use (eg. without
-# having to mess around with ANSI escape sequences). This code is public
-# domain - there is no license except that you must leave this header.
-#
-# Copyright (C) 2008 Brian Nez <thedude at bri1 dot com>
-#
-# With modifications
-#           (C) 2013 Paul M <pmathieu@willowgarage.com>
-
-codeCodes = {
-    'black':     '0;30',     'bright gray':   '0;37',
-    'blue':      '0;34',     'white':         '1;37',
-    'green':     '0;32',     'bright blue':   '1;34',
-    'cyan':      '0;36',     'bright green':  '1;32',
-    'red':       '0;31',     'bright cyan':   '1;36',
-    'purple':    '0;35',     'bright red':    '1;31',
-    'yellow':    '0;33',     'bright purple': '1;35',
-    'dark gray': '1;30',     'bright yellow': '1;33',
-    'normal':    '0'
-}
-
-
-def printc(text, color):
-    """Print in color."""
-    if sys.stdout.isatty():
-        print("\033["+codeCodes[color]+"m"+text+"\033[0m")
-    else:
-        print(text)
-
-
-def print_test(msg):
-    printc(msg, 'yellow')
-
-
-def print_err(msg):
-    printc('  ERR: ' + msg, 'red')
-
 ###
 ###
 from rosdep2.sources_list import load_cached_sources_list, DataSourceMatcher, SourcesListLoader, CachedDataSource
@@ -88,7 +50,7 @@ def create_default_sources():
         index = yaml.load(f.read())
         for distro in index['distributions']:
             distfile = 'file://'+basedir+'/'+distro+'/distribution.yaml'
-            print_test("loading %s" % (distfile))
+            print("loading %s" % (distfile))
             rds = RosDistroSource(distro)
             rosdep_data = get_gbprepo_as_rosdep_data(distro)
             sources.append(CachedDataSource('yaml', distfile, [distro], rosdep_data))
@@ -105,9 +67,9 @@ def create_default_sources():
 def check_duplicates(sources):
     ret = True
     # output debug info
-    print_test("checking sources")
+    print("checking sources")
     for source in sources:
-        print_test("- %s" % source.url)
+        print("- %s" % source.url)
 
     # create loopkup
     sources_loader = SourcesListLoader(sources)
@@ -117,14 +79,14 @@ def check_duplicates(sources):
     db_name_view = dict()
 
     # check if duplicates
-    print_test("checking duplicates")
+    print("checking duplicates")
     view = lookup.get_rosdep_view(DEFAULT_VIEW_KEY, verbose=None) # to call init
     for view_key in lookup.rosdep_db.get_view_dependencies(DEFAULT_VIEW_KEY):
         db_entry=lookup.rosdep_db.get_view_data(view_key)
-        print_test("* %s" % view_key)
+        print("* %s" % view_key)
         for dep_name, dep_data in db_entry.rosdep_data.items():
             if dep_name in db_name_view:
-                print_err("%s is multiply defined in\n\t%s and \n\t%s\n"%(dep_name, db_name_view[dep_name], view_key))
+                print("%s is multiply defined in\n\t%s and \n\t%s\n"%(dep_name, db_name_view[dep_name], view_key))
                 ret = False
             db_name_view[dep_name] = view_key
     return ret
@@ -135,9 +97,9 @@ def main(infile):
     sources = create_default_sources()
     matcher = DataSourceMatcher.create_default()
 
-    print_test("default sources")
+    print("default sources")
     for source in sources:
-        print_test("- %s" % source.url)
+        print("- %s" % source.url)
 
     # replace with infile
     for filename in infile:
@@ -160,7 +122,7 @@ def main(infile):
                 ['kinetic', 'ubuntu', 'xenial'],
                 ['lunar', 'ubuntu', 'xenial']]:
         matcher.tags = tag
-        print_test('checking with %s'%(matcher.tags))
+        print('checking with %s'%(matcher.tags))
         sources = [x for x in sources if matcher.matches(x)]
         ret = ret & check_duplicates(sources)
     return ret
