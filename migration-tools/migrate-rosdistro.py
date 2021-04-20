63c7e5a92f0aeef74bc055953176ade2911c0ecf
Scott K Logan <logans@cottsay.net>
2021-04-20 11:22:49 -0700

    Update rosdep only once during migration (#29213)

--- migration-tools/migrate-rosdistro.py
+++ migration-tools/migrate-rosdistro.py
@@ -114,6 +114,12 @@ repositories_with_errors = []
 workdir = tempfile.mkdtemp()
 os.chdir(workdir)
 os.environ['ROSDISTRO_INDEX_URL'] = rosdistro_index_url
+os.environ['BLOOM_SKIP_ROSDEP_UPDATE'] = '1'
+
+# This call to update rosdep is critical because we're setting
+# ROSDISTRO_INDEX_URL above and also suppressing the automatic
+# update in Bloom itself.
+subprocess.check_call(['rosdep', 'update'])
 
 for repo_name in sorted(new_repositories + repositories_to_retry):
     try:
